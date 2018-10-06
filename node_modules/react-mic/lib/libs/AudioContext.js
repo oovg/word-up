9eeb4be94f708c0bb9ca914fb2e5b799d302c6a2
oovg <office@vengist.com>
2018-10-06 00:50:14 -0700

    first commit

---
+++ node_modules/react-mic/lib/libs/AudioContext.js
@@ -0,0 +1,22 @@
+"use strict";
+
+exports.__esModule = true;
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var analyser = audioCtx.createAnalyser();
+
+var AudioContext = {
+  getAudioContext: function getAudioContext() {
+    return audioCtx;
+  },
+  getAnalyser: function getAnalyser() {
+    return analyser;
+  },
+  decodeAudioData: function decodeAudioData() {
+    audioCtx.decodeAudioData(audioData).then(function (decodedData) {
+      // use the decoded data here
+    });
+  }
+};
+
+exports.default = AudioContext;
+module.exports = exports["default"];
---
+++ src/configs/config-main.js
@@ -0,0 +1,8 @@
+/* Main app configs go here */
+
+export const appConfig = {
+  name: 'YourReactDapp',
+  shortName: 'YourReactDapp',
+  description: 'A foundational boilerplate for a React Dapp For Ethereum',
+  splashScreenBackground: '#ffffff'
+}
