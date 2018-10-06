5bbcb0d4d277d9bdead8da0a39590322be4d1ae8
Victor Truong <me@ifvictr.com>
2018-10-06 12:06:52 -0700

    Add mock data

---
+++ src/data/prompts.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    prompt: {
+      id: 1,
+      body: 'The dwarven race was known for it\'s large, elaborate works of art. As technology advanced they got bigger and more elaborate. Then one day a dwarven clan bought some terraforming equipment.'
+    },
+    marketCap: 8452.397
+  }
+]
