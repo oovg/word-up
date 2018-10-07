c67e4e49491829fe473f80c0800f3b87a93fcf01
Victor Truong <me@ifvictr.com>
2018-10-06 19:37:10 -0700

    Fetch from store by prmompt id

--- src/containers/PromptView/index.js
+++ src/containers/PromptView/index.js
@@ -1,23 +1,24 @@
 import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
 import AppBar from '../../components/AppBar'
 import Prompt from '../../components/Prompt'
 
 import './styles.scss'
 
 class PromptView extends Component {
-    componentDidMount() {
-        // TODO: Fetch prompt object using this.props.match.params.id
-    }
-
     render() {
         console.log(this.props.match.params)
         return (
             <Fragment>
                 <AppBar />
-                <Prompt />
+                <Prompt data={this.props.prompt} />
             </Fragment>
         )
     }
 }
 
-export default PromptView
+const mapStateToProps = (state, ownProps) => ({
+    prompt: state.prompts.prompts.find(prompt => prompt.id == ownProps.match.params.id)
+})
+
+export default connect(mapStateToProps)(PromptView)
