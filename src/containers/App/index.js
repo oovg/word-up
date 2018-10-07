0d04ef6453f19196a8ae7dccd38754182a6eb9fd
dbe <davidbrianethier@gmail.com>
2018-10-07 02:26:36 -0700

    Doing surgery on the drawer

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
@@ -5,12 +6,35 @@ import theme from 'configs/theme/config-theme'
 import HomeView from 'containers/HomeView'
 import PromptView from 'containers/PromptView'
 
+
+import { Drawer } from '@material-ui/core'
+import Close from '@material-ui/icons/Close'
+import PromptComposer from '../../components/PromptComposer'
+
 import './styles.scss' // global styles
 
 class App extends Component {
+  drawer() {
+    return (
+      <Drawer width="50%" className="drawer--prompt" anchor="right" open={this.props.drawerOpen} onClose={this.toggleDrawer}>
+        <button
+          className="button plain button--close"
+          tabIndex={0}
+          role="button"
+          onClick={this.toggleDrawer}
+          onKeyDown={this.toggleDrawer}
+        >
+          <Close />
+        </button>
+        <PromptComposer />
+      </Drawer>
+    )
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        { this.drawer() }
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={HomeView} />
@@ -22,4 +46,10 @@ class App extends Component {
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    drawerOpen: state.ui.rightDrawerIsOpen
+  }
+}
+
+export default connect(mapStateToProps, null)(App)
--- src/core/types.js
+++ src/core/types.js
@@ -1,5 +1,7 @@
 import keyMirror from 'keymirror'
 
 export default keyMirror({
-  ADD_PROMPT: null
+  ADD_PROMPT: null,
+  OPEN_RIGHT_DRAWER: null,
+  CLOSE_RIGHT_DRAWER: null
 })
