c2fc8bc0f99eb874e2089f3c94ba15f15e24631e
dbe <davidbrianethier@gmail.com>
2018-10-06 17:20:30 -0700

    Fixing conflicts

--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,63 +1,42 @@
-<<<<<<< HEAD
 import { connect } from 'react-redux'
-=======
 import { Drawer } from '@material-ui/core'
->>>>>>> 58bc39fd3fc1fb48ab989b787564a7aeb130d3bb
 import React, { Component } from 'react'
 import PromptComposer from '../../components/PromptComposer'
 import PromptList from '../../components/PromptList'
 import AppBar from '../../components/AppBar'
 import Api from '../../api/api'
-<<<<<<< HEAD
 import { addPrompt } from '../../core/actions/actions-prompts'
-
-class HomeView extends Component {
-  constructor(props) {
-    super(props)
-    // this.api = new Api()
-    window.oreo = () => this.props.addPrompt({
-      body: 'from oreo',
-      id: 3,
-      marketCap: 999
-    })
-  }
-
-=======
 import Add from '@material-ui/icons/Add'
-
 import './styles.scss'
 
 class HomeView extends Component {
   constructor() {
     super()
-    this.api = new Api()
   }
 
   state = {
     top: false,
     left: false,
     bottom: false,
-    right: false,
+    right: false
   };
 
   toggleDrawer = (side, open) => () => {
     this.setState({
-      [side]: open,
-    });
+      [side]: open
+    })
   };
 
->>>>>>> 58bc39fd3fc1fb48ab989b787564a7aeb130d3bb
   render() {
     return (
       <div>
         <Drawer width="50%" containerClassName="drawer--party" className="drawer--prompt" anchor="right" open={this.state.right} onClose={this.toggleDrawer('right', false)}>
-        <div
-          tabIndex={0}
-          role="button"
-          onClick={this.toggleDrawer('right', false)}
-          onKeyDown={this.toggleDrawer('right', false)}
-        >
-        </div>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('right', false)}
+            onKeyDown={this.toggleDrawer('right', false)}
+          />
           <PromptComposer />
         </Drawer>
         <AppBar />
@@ -68,11 +47,7 @@ class HomeView extends Component {
           </div>
         </div>
         <div className="prompts">
-<<<<<<< HEAD
           <PromptList prompts={this.props.prompts} />
-=======
-          <PromptList prompts={this.api.prompts()} />
->>>>>>> 58bc39fd3fc1fb48ab989b787564a7aeb130d3bb
         </div>
       </div>
     )
