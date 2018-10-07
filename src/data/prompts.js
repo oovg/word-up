1d7df287d87891e5780bf8ef75cab2cde9d6da45
oovg <office@vengist.com>
2018-10-06 19:01:22 -0700

    adds data passages

--- src/data/prompts.js
+++ src/data/prompts.js
@@ -4,12 +4,30 @@ module.exports = {
       {
         id: 1,
         body: 'The dwarven race was known for it\'s large, elaborate works of art. As technology advanced they got bigger and more elaborate. Then one day a dwarven clan bought some terraforming equipment.',
-        marketCap: 8452.397
+        marketCap: 8452.397,
+        passages:
+        [
+          {
+            body: 'What happened next, ',
+          },
+          {
+            body: 'would change everything.',
+          }
+        ]
       },
       {
         id: 2,
         body: 'my pet rock',
-        marketCap: 8452.397
+        marketCap: 8452.397,
+        passages:
+        [
+          {
+            body: 'What happened next, ',
+          },
+          {
+            body: 'would change everything.',
+          }
+        ]
       }
     ]
 }
