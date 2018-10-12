8c7fed15d6099ebd1113407cc8f305592c1d735b
skuhlmann <samkuhlmann@gmail.com>
2018-10-12 15:08:52 -0600

    Adds tcrFactoryService

--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,31 +1,42 @@
-import { connect } from 'react-redux'
-import React, { Component } from 'react'
-import Add from '@material-ui/icons/Add'
-import constants from 'core/types'
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import Add from '@material-ui/icons/Add';
+import constants from 'core/types';
 
-import PromptList from '../../components/PromptList'
-import AppBar from '../../components/AppBar'
-import { addPrompt } from '../../core/actions/actions-prompts'
-import { openRightDrawer, updateDrawerContext } from '../../core/actions/actions-ui'
-import TcrFactory from '../../api/TcrFactory'
+import PromptList from '../../components/PromptList';
+import AppBar from '../../components/AppBar';
+import { addPrompt } from '../../core/actions/actions-prompts';
+import {
+  openRightDrawer,
+  updateDrawerContext,
+} from '../../core/actions/actions-ui';
+// import TcrFactory from '../../api/TcrFactory'
 
-import './styles.scss'
+import TcrFactoryService from '../../utils/TcrFactoryWeb3';
+
+import './styles.scss';
 
 class HomeView extends Component {
   constructor(props) {
-    super(props)
-    const account = '0xd66e018cc12b0e35dc1abf0991cef06a6bd295b9'
-    const contractAddr = '0x73f24e09486db7e69705f465832be91b13e67917'
-    const erc20Addr = '0x08cd0c6dc1ff4614053dca4a4a6aa39655d19b32'
-    const factory = new TcrFactory(web3, contractAddr)
+    super(props);
+    const account = '0xd66e018cc12b0e35dc1abf0991cef06a6bd295b9';
+    const contractAddr = '0x73f24e09486db7e69705f465832be91b13e67917';
+    const erc20Addr = '0x08cd0c6dc1ff4614053dca4a4a6aa39655d19b32';
+    // const factory = new TcrFactory(web3, contractAddr)
 
+    this.factoryService = new TcrFactoryService();
+    this.loadContract();
     // factory.createTCR('content', 2, erc20Addr, 0)
   }
 
+  loadContract = async () => {
+    const contract = await this.factoryService.initContracts();
+    console.log(contract);
+  };
 
   openPromptComposer() {
-    this.props.openRightDrawer()
-    this.props.updateDrawerContext(constants.PROMPT_COMPOSER, {})
+    this.props.openRightDrawer();
+    this.props.updateDrawerContext(constants.PROMPT_COMPOSER, {});
   }
 
   render() {
@@ -35,20 +46,25 @@ class HomeView extends Component {
         <div className="toolbar">
           <div className="contents">
             <h2>Viewing Prompts</h2>
-            <button className="button" onClick={this.openPromptComposer.bind(this)}><Add /> Add A Prompt</button>
+            <button
+              className="button"
+              onClick={this.openPromptComposer.bind(this)}
+            >
+              <Add /> Add A Prompt
+            </button>
           </div>
         </div>
         <div className="prompts">
           <PromptList prompts={this.props.prompts} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ prompts: state.prompts })
+const mapStateToProps = (state) => ({ prompts: state.prompts });
 
 export default connect(
   mapStateToProps,
-  { addPrompt, openRightDrawer, updateDrawerContext }
-)(HomeView)
+  { addPrompt, openRightDrawer, updateDrawerContext },
+)(HomeView);
---
+++ src/utils/TcrFactoryWeb3.js
@@ -0,0 +1,81 @@
+import TrcFactoryAbi from './TcrFactoryAbi.json';
+import { getWeb3ServiceInstance } from './Web3Service';
+
+export default class TrcFactoryService {
+  web3Service;
+  trcFactoryContract;
+
+  constructor() {
+    this.web3Service = getWeb3ServiceInstance();
+    this.tokenAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+  }
+
+  async initContracts() {
+    console.log(TrcFactoryAbi);
+    return (this.trcFactoryContract = await this.web3Service.initContract(
+      TrcFactoryAbi.abi,
+      this.tokenAddress,
+    ));
+  }
+
+  async buy(hashId, ercBuyAmount) {
+    return await this.tcrFactoryContract.methods
+      .buy(hashId, ercBuyAmount)
+      .call();
+  }
+
+  async sell(hashId, ercSellAmount) {
+    return await this.tcrFactoryContract.methods
+      .sell(hashId, ercSellAmount)
+      .call();
+  }
+
+  async createTcr(content, ratio, erc20, startingBalance) {
+    return await this.tcrFactoryContract.methods
+      .createTcr(content, ratio, erc20, startingBalance)
+      .call();
+  }
+
+  async renounceOwnership() {
+    return await this.tcrFactoryContract.methods.renounceOwnership().call();
+  }
+
+  async transferOwnership(addr) {
+    return await this.tcrContract.methods.transferOwnership(addr).call();
+  }
+
+  async calculatePurchaseReturn(
+    supply,
+    connectorBalance,
+    connectorWeight,
+    depositAmount,
+  ) {
+    return await this.tcrFactoryContract.methods
+      .calculatePurchaseReturn(
+        supply,
+        connectorBalance,
+        connectorWeight,
+        depositAmount,
+      )
+      .call();
+  }
+
+  async calculateSellReturn(
+    supply,
+    connectorBalance,
+    connectorWeight,
+    sellAmount,
+  ) {
+    return await this.tcrFactoryContract.methods
+      .calculateSellReturn(
+        supply,
+        connectorBalance,
+        connectorWeight,
+        sellAmount,
+      )
+      .call();
+  }
+  async getContent(txHash) {
+    return await this.tcrFactoryContract.methods.getContent(txHash).call();
+  }
+}
