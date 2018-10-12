384068a2eed8084bd07f6bf5c121717648d14e9a
oovg <office@vengist.com>
2018-10-12 14:55:05 -0700

    adds handleClickOutside wallet

--- src/components/Wallet/index.js
+++ src/components/Wallet/index.js
@@ -20,14 +20,29 @@ class Wallet extends React.Component {
     this.setState({walletIsOpen: false})
   }
 
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  handleClick = (e) => {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.closeWallet();
+  }
+
   render() {
     return (
-      <div className="wallet">
+      <div className="wallet" >
         <div className="wallet--button" onClick={this.openWallet}>
           <Blockie seed="0xfe89f7s098guoirshg19p843yp8tup84u"/>
         </div>
         { this.state.walletIsOpen &&
-        <div className="wallet--view">
+        <div className="wallet--view" ref={node => this.node = node}>
           <button className="close button" onClick={this.closeWallet}><Close /></button>
           <p><span>Account</span>0xfe89f7s098guoirshg19p843yp8tup84u</p>
           <h4>Balances</h4>
